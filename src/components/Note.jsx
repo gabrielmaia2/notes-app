58d7b20e899c042f1a8688d13a08a1d531bef689
Gabriel Maia Gondim <41156120+gabrielmaia2@users.noreply.github.com>
2021-09-11 10:08:57 -0300

    Create Note.jsx

---
+++ src/components/Note.jsx
@@ -0,0 +1,42 @@
+import {
+  faSave, faTrashAlt,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { Component } from 'react';
+import '../styles/note.css';
+
+export default class Note extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      content: '',
+    };
+
+    this.handleTitleInput = this.handleTitleInput.bind(this);
+    this.handleContentInput = this.handleContentInput.bind(this);
+  }
+
+  handleTitleInput(event) {
+    this.setState({ title: event.target.value });
+  }
+
+  handleContentInput(event) {
+    this.setState({ content: event.target.value });
+  }
+
+  render() {
+    const { title, content } = this.state;
+
+    return (
+      <div className="note">
+        <input type="text" className="title" placeholder="Title" value={title} onChange={this.handleTitleInput} />
+        <input type="text" className="content" placeholder="Write something..." value={content} onChange={this.handleContentInput} />
+        <div className="icons">
+          <FontAwesomeIcon className="icon save" icon={faSave} />
+          <FontAwesomeIcon className="icon delete" icon={faTrashAlt} />
+        </div>
+      </div>
+    );
+  }
+}
