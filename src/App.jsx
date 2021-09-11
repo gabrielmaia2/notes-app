6487877bead9b22dce99ad64b42bd07ab86c20bc
Gabriel Maia Gondim <41156120+gabrielmaia2@users.noreply.github.com>
2021-09-11 10:06:31 -0300

    Update files
    Change .js to .jsx
    Update files to follow eslint rules

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import NotesList from './components/NotesList';
+
+function App() {
+  return (
+    <div className="App">
+      Hello world
+    </div>
+  );
+}
+
+export default App;
