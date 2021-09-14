80a0c0ff111bcc6830dfe9cf3ecf9509398c5ed7
Gabriel Maia Gondim <41156120+gabrielmaia2@users.noreply.github.com>
2021-09-14 13:03:58 -0300

    Create NotePreview component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import NotePreview from './components/NotePreview';
 
 import './App.css';
 
@@ -8,9 +9,12 @@ class App extends Component {
   }
 
   render() {
+    const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc finibus urna ornare convallis consectetur. Sed a scelerisque leo, id blandit ipsum. Integer eget ipsum a nisl volutpat ultrices. Mauris convallis, nisi vel vulputate tincidunt, felis sapien aliquam ligula, ullamcorper semper ipsum quam at erat. Fusce semper lacus iaculis velit placerat elementum. Mauris in quam sollicitudin, varius urna sit amet, mollis leo. Integer ultrices, ipsum eget consectetur pharetra, sapien ante ornare lectus, sed dictum eros leo et orci. Nullam elit metus, sagittis non vehicula in, interdum vitae turpis. Integer at urna ut arcu maximus egestas. Pellentesque pellentesque erat eu sem condimentum, ut euismod mi posuere. Morbi faucibus risus non blandit pharetra. Donec rhoncus quam eu nisl blandit congue. In blandit, urna quis venenatis sodales, quam dui pellentesque nunc, non posuere nibh elit in risus. Proin gravida bibendum ante, quis elementum turpis luctus vitae. Sed sagittis metus et sapien pulvinar, ut accumsan augue molestie.';
+
     return (
       <div className="App">
         Hello World
+        <NotePreview id="" title="Title" content={lorem} onSave={() => {}} onDelete={() => {}} />
       </div>
     );
   }
---
+++ src/components/NotePreview.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import '../styles/note-preview.css';
+
+// eslint-disable-next-line react/prefer-stateless-function
+export default class NotePreview extends Component {
+  render() {
+    const { title, content } = this.props;
+
+    return (
+      <div className="note">
+        <div className="title text">{title}</div>
+        <div className="content text">{content}</div>
+      </div>
+    );
+  }
+}
+
+NotePreview.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+};
