100d7f1da090b70a9bef2c05f1351014ae829ade
Gabriel Maia Gondim <41156120+gabrielmaia2@users.noreply.github.com>
2021-09-17 15:33:51 -0300

    Update startBackend.sh
    Added verbosity on start-backend script

--- startBackend.sh
+++ startBackend.sh
@@ -1,6 +1,15 @@
-if [ -f .env ]
+if [ -f .env ];
 then
-  export $(cat .env | xargs)
+  export $(cat .env | xargs);
+else
+  echo "ERROR: Could not find .env file.";
+  exit 1;
+fi
+
+if [ -z ${BACKEND_PATH+x} ];
+then
+  echo "ERROR: BACKEND_PATH environment variable not set.";
+  exit 1;
 fi
 
 cd "$BACKEND_PATH";
